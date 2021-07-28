480fcad2f6ec6c9db86f82ce3db81be17348a3d3
Tutu <turquetivitor@gmai.com>
2021-07-28 02:19:05 -0300

    rest funcionando

--- demoprojec/src/main/java/com/example/demo/EventoController.java
+++ demoprojec/src/main/java/com/example/demo/EventoController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,39 @@ public class EventoController {
     public Evento saveEvento(@RequestBody Evento evento){
         return repo.save(evento);
     }
+
+    @GetMapping("/evento/{id}")
+    public Evento getEventoById(@PathVariable String id){
+        return repo.findById(id).get();
+    }
+
+    @DeleteMapping("/evento/{id}")
+    public void deleteEventoById(@PathVariable String id){
+        repo.deleteById(id);
+    }
+
+    @PutMapping("/evento/{id}")
+    public Evento updateEvento(@RequestBody Evento evento,@PathVariable String id) throws Exception {
+        if (repo.findById(id).isPresent()){
+            return repo.save(evento);
+        }else{
+            throw new Exception("Evento não encontrado");
+        }
+    }
+
+
+    @PostMapping("/evento/{id}/inserePessoa")
+    public Evento addPessoaEvento(@RequestBody Pessoa pessoa,@PathVariable String id) throws Exception {
+        if (repo.findById(id).isPresent()){
+            Evento event = repo.findById(id).get();
+            event.getPessoas().add(pessoa);
+            return repo.save(event);
+        }else{
+
+            throw new Exception("Evento não encontrado");
+        }
+    }
+    
+
 
 }
