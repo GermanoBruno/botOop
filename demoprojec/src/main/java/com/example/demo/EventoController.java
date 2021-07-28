a53ff7701f2a8d0baa3187f0873244ded45ec5ee
Tutu <turquetivitor@gmai.com>
2021-07-28 16:24:00 -0300

    funcionalidades ok e add error message

--- demoprojec/src/main/java/com/example/demo/EventoController.java
+++ demoprojec/src/main/java/com/example/demo/EventoController.java
@@ -44,14 +44,72 @@ public class EventoController {
     public Evento addPessoaEvento(@RequestBody Pessoa pessoa,@PathVariable String id) throws Exception {
         if (repo.findById(id).isPresent()){
             Evento event = repo.findById(id).get();
-            event.getPessoas().add(pessoa);
+
+            List<Pessoa> pessoas = event.getPessoas();
+
+            for (int i = 0; i < pessoas.size();i++){
+                if (pessoa.getNome().equals(pessoas.get(i).getNome())){
+                    throw new Exception("Pessoa ja adicionada");
+                }
+            }
+
+            for (int i = 0; i < pessoa.getHoras_dosponiveis().size();i++){
+                if (! event.getDias_da_semana().contains(pessoa.getHoras_dosponiveis().get(i).getDia())){
+                    throw new Exception("Dia da semana inválido");
+                }
+                for (int j = 0; j < pessoa.getHoras_dosponiveis().get(i).getHorarios().size();j++){
+                    if (pessoa.getHoras_dosponiveis().get(i).getHorarios().get(j).compareTo(event.getNao_antes()) < 0 || pessoa.getHoras_dosponiveis().get(i).getHorarios().get(j).compareTo(event.getNao_depois()) > 0){
+                        throw new Exception("Horario inválido");
+                    }
+                }
+
+            }
+
             return repo.save(event);
         }else{
 
             throw new Exception("Evento não encontrado");
         }
     }
-    
+
+    @PutMapping("/evento/{id}/updatehorarios/{nomePessoa}")
+    public Evento updatePessoaEvento(@RequestBody Pessoa pessoa,@PathVariable String id,@PathVariable String nomePessoa) throws Exception {
+        if (repo.findById(id).isPresent()){
+            Evento event = repo.findById(id).get();
+
+            List<Pessoa> pessoas = event.getPessoas();
+            boolean flagEncontrou = false;
+
+            for (int i = 0; i < pessoas.size();i++){
+                if (nomePessoa.equals(pessoas.get(i).getNome())){
+                    for (int k = 0; k < pessoa.getHoras_dosponiveis().size();k++){
+                        if (! event.getDias_da_semana().contains(pessoa.getHoras_dosponiveis().get(k).getDia())){
+                            throw new Exception("Dia da semana inválido");
+                        }
+                        for (int j = 0; j < pessoa.getHoras_dosponiveis().get(k).getHorarios().size();j++){
+                            if (pessoa.getHoras_dosponiveis().get(k).getHorarios().get(j).compareTo(event.getNao_antes()) < 0 || pessoa.getHoras_dosponiveis().get(k).getHorarios().get(j).compareTo(event.getNao_depois()) > 0){
+                                throw new Exception("Horario inválido");
+                            }
+                        }
+
+                    }
+                    pessoas.set(i,pessoa);
+                    flagEncontrou = true;
+                }
+            }
+
+            if (!flagEncontrou){
+                throw new Exception("Pessoa não encontrada");
+            }
+
+            event.setPessoas(pessoas);
+
+            return repo.save(event);
+        }else{
+
+            throw new Exception("Evento não encontrado");
+        }
+    }
 
 
 }
